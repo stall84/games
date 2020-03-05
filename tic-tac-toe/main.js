d8ef2304ceeed121786b08ead329ec92400d5c54
Michael Stallings <stall84@gmail.com>
2020-03-05 14:21:42 -0500

    made some progress on tic-tac-toe. created onclick attributes for the buttons

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,6 +1,27 @@
+// To-Do List:
+// Assign X and O to player and Computer
+// Make click function to store/display which square user clicked in & display his 'playing piece x/o'
+
+
+
 var content = document.getElementById('content');
 
-var ticTacToe = "Replace this with your own abstraction of Tic Tac Toe"
+var ticTacToe;
+
+// creating array to store the play-piece symbols
+var playPiece = ["X","O"];
+
+// Just going to declare two variables to hold info for both players
+var playerX;
+var playerO;
+
+function clickFunc (clicked_id) {
+
+    alert(clicked_id);
+
+
+}
+
 
 content.innerHTML = renderGame(ticTacToe);
 
@@ -8,25 +29,27 @@ content.innerHTML = renderGame(ticTacToe);
 
 
 function renderGame(game) {
-    // Change this render function to use the "game" parameter
+
+    // Created onclick attributes ref'ing the click function we're making above. Also added the 'this.id' parameter
+    // to test out and give reference to each button.
 
     return `
         <div class="container d-flex flex-column justify-content-start align-items-center">
             <h4>It's player O's turn!</h4>
             <div class="w-50 text-center">
-                <button>X</button>
-                <button>O</button>
-                <button>X</button>
+                <button id="11" onclick="clickFunc(this.id)">-</button>
+                <button id="12" onclick="clickFunc(this.id)">-</button>
+                <button id="13" onclick="clickFunc(this.id)">-</button>
             </div>
             <div class="w-50 text-center">
-                <button>-</button>
-                <button>-</button>
-                <button>-</button>
+                <button onclick="clickFunc">-</button>
+                <button onclick="clickFunc">-</button>
+                <button onclick="clickFunc">-</button>
             </div>
             <div class="w-50 text-center">
-                <button>O</button>
-                <button>-</button>
-                <button>X</button>
+                <button onclick="clickFunc">-</button>
+                <button onclick="clickFunc">-</button>
+                <button onclick="clickFunc">-</button>
             </div>
         </div>
     `
